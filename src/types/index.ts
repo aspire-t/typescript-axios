d77a6806b8a921cc0fb372dd024ccd292ef54d92
wangzhentao <854466391@qq.com>
2019-12-03 09:50:12 +0800

    xhr demo

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,22 @@
-export type Method = 'get' | 'GET' | 'delete' | 'DELETE' | 'head' | 'HEAD' | 'options' | 'OPTIONS' | 'post' | 'POST' | 'put' | 'PUT' | 'patch' | 'PATCH'
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string,
-  methods?: Method,
-  data?: any,
+  url: string
+  method?: Method
+  data?: any
   params?: any
 }
