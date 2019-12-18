f62b93530a48a7e6e0523d5c485679b05a8f91cd
aspire-t <zhijianwubu@gmail.com>
2019-12-18 11:41:40 +0800

    handle error

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,4 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/header'
-// import axios from './index'
-import { AxiosResponse } from './types/index'
-
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  // 这里结构赋值，给headers一个默认值，让headers不是undefined，这样可以确保headers['Content-Type']有一个默认值
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+import axios from './axios'
+export * from './types'
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import { rejects } from 'assert'
 import { parseHeaders } from './helpers/header'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -42,11 +42,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     // 网络错误
     request.onerror = function handleError() {
-      reject(new Error('NetWork Error'))
+      reject(createError('NetWork Error', config, null, request))
     }
 
     request.ontimeout = function handleTimeout() {
-      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
     }
 
     Object.keys(headers).forEach(name => {
@@ -64,7 +64,15 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (response.status >= 200 && response.status < 300) {
         resolve(response)
       } else {
-        reject(new Error(`Request failed with status code ${response.status}`))
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
       }
     }
   })
